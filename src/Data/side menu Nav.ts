af487114c2206d7149ed4fc8fe57857c90c4678d
MaiShelbayeh <s11923686@najah.stu.edu>
2025-04-16 11:15:08 +0300

    side menu

--- src/Data/side menu Nav.ts
+++ src/Data/side menu Nav.ts
@@ -1,18 +1,18 @@
 
 import {
-    HomeIcon,
-    ChartBarIcon,
-    ClipboardListIcon,
-    TemplateIcon,
-    LocationMarkerIcon,
-    CogIcon,
-  } from '@heroicons/react/outline';
+  HomeIcon,
+  ChartBarIcon,
+  ClipboardListIcon,
+  TemplateIcon,
+  LocationMarkerIcon,
+  CogIcon,
+} from '@heroicons/react/outline';
 import { MenuItem } from '../Types';
 export const getMenuAsText = (): MenuItem[] => [
-  { id: 1, title: 'Home', icon: HomeIcon, path: '/' },
-  { id: 2, title: 'Performance Indicators', icon: ChartBarIcon, path: '/analytics' },
-  { id: 3, title: 'Reports', icon: ClipboardListIcon, path: '/profile' },
-  { id: 4, title: 'Projects', icon: TemplateIcon, path: '/projects' },
-  { id: 5, title: 'Maps', icon: LocationMarkerIcon, path: '/map' },
-  { id: 6, title: 'Settings', icon: CogIcon, path: '/settings' },
+  { id: 1, title: 'Home', icon: HomeIcon, path: '/home' },
+  { id: 2, title: 'Statistics', icon: ChartBarIcon, path: '/statistics' },
+  { id: 3, title: 'Reports', icon: ClipboardListIcon, path: '/Reports' },
+{ id: 4, title: 'Projects', icon: TemplateIcon, path: '/projects' },
+{ id: 5, title: 'Maps', icon: LocationMarkerIcon, path: '/map' },
+{ id: 6, title: 'Settings', icon: CogIcon, path: '/settings' },
 ];
